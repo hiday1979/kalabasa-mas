f4ec0adac77fb45b8d4bd82d06bf9a75a292e8aa
WigiTech <accounts@wigitech.com>
2018-04-24 13:17:47 +0300

    progress bar opacity/width

--- src/app/main-pic/main-pic.component.ts
+++ src/app/main-pic/main-pic.component.ts
@@ -18,30 +18,15 @@ export class MainPicComponent implements OnInit {
   }
 
   move() {
-    document.getElementById('myMainBar').addEventListener('click', function (e) {
-      const x = e.pageX - this.offsetLeft, // or e.offsetX (less support, though)
-          y = e.pageY - this.offsetTop,  // or e.offsetY
-          clickedValue = x * this.offsetLeft / this.offsetWidth,
-          isClicked = clickedValue <= x;
-
-
-      if (isClicked) {
-          // alert('You clicked within the value range at: ' + clickedValue);
-          document.getElementById('mainPic').style.width = '' + clickedValue + '%';
-          document.getElementById('myBar').style.width = '' + clickedValue + '%';
-      }
-  });
-    const elem = document.getElementById('myBar');
-    let width = 1;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-        }
+    const bar = <HTMLInputElement> document.getElementById('myBar');
+    bar.addEventListener('change', function () {
+      document.getElementById('mainPic').style.width = '' + bar.value + '%';
+    };
+    const rgbBar = <HTMLInputElement> document.getElementById('myRgbBar');
+    rgbBar.addEventListener('change', function () {
+      document.getElementById('mainPic').style.opacity = rgbBar.value / 100;
+    };
+    }
     }
 }
 
-}
